9ee09494ff9cb9add562ab4217890df6a0e043d9
Eric-McKinney <ericmckinney.md@gmail.com>
2025-07-13 21:24:57 -0400

    Initial commit

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = function()
+		require("nvim-treesitter.install").update({ with_sync = true })()
+	end,
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        
+        configs.setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "diff",
+                "html",
+                "javascript",
+                "jsdoc",
+                "json",
+                "jsonc",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "printf",
+                "python",
+                "query",
+                "regex",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "xml",
+                "yaml",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
+}
