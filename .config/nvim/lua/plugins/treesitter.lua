9ac37bf1c6225559eb3651d775e962e180ce61fa
Eric-McKinney <ericmckinney.md@gmail.com>
2025-07-19 18:56:03 -0400

    Add nix to languages

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -26,6 +26,7 @@ return {
                 "luap",
                 "markdown",
                 "markdown_inline",
+                "nix",
                 "printf",
                 "python",
                 "query",
